81a724cccf0da40608c76c79dcd35bb80bc26bdf
Alexey Nazarov <naz.arrow.lex@gmail.com>
2025-09-28 15:26:25 +0300

    added classwork package

---
+++ src/classwork/day7/DayScan.java
@@ -0,0 +1,18 @@
+package classwork.day7;
+
+import java.util.Scanner;
+
+public class DayScan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String incoming = scanner.nextLine();
+            if (incoming.equalsIgnoreCase("STOP")) {
+                break;
+            }
+            System.out.printf("Just got '%s' text!", incoming);
+        }
+
+    }
+}
