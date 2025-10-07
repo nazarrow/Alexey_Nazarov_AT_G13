70d801ea2122d51dec5889f81206f5bc74f5dd36
Alexey Nazarov <naz.arrow.lex@gmail.com>
2025-10-07 12:41:32 +0300

    added solution for 17-20 tasks

---
+++ src/homework/day8/Task18Cars.java
@@ -0,0 +1,39 @@
+package homework.day8;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task18Cars {
+    public static void main(String[] args) {
+
+        List<Cars> cars = new ArrayList<>();
+        cars.add(new Cars("Toyota", "Corolla"));
+        cars.add(new Cars("Ford", "Mustang"));
+        cars.add(new Cars("BMW", "X5"));
+
+        for (Cars c : cars) {
+            System.out.println(c.getBrand() + ":" + c.getModel());
+        }
+
+        Map<String, Cars> stringCarsMap = new HashMap<>();
+        for (Cars c : cars) {
+            stringCarsMap.put(c.getBrand(), c);
+        }
+
+        for (String brand : stringCarsMap.keySet()) {
+            System.out.println(brand);
+        }
+
+        for (Cars model : stringCarsMap.values()) {
+            System.out.println(model);
+        }
+        System.out.println();
+
+        for (Map.Entry<String, Cars> entry : stringCarsMap.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+    }
+}
