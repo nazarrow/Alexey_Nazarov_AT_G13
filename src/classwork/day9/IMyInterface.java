0d3f929563b8d4f19f84839480a2eaaec854e1ea
Alexey Nazarov <naz.arrow.lex@gmail.com>
2025-10-08 20:55:14 +0300

    added classwork day 9

---
+++ src/classwork/day9/IMyInterface.java
@@ -0,0 +1,5 @@
+package classwork.day9;
+
+public interface IMyInterface {
+    void doSomething(String string);
+}
---
+++ src/classwork/day9/MyStream2.java
@@ -0,0 +1,21 @@
+package classwork.day9;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MyStream2 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("мама", "мыла", "раму", "мама", "чисто");
+
+        System.out.println(list.stream().findFirst().orElse("Pusto"));
+
+        System.out.println(list.stream().filter("мама"::equals).findFirst().get());
+
+        System.out.println(list.stream().skip(4).findFirst().get());
+
+        System.out.println(list.stream().skip(2).limit(2).toList());
+
+        System.out.println(list.stream().distinct().filter(string -> string.contains("м")).toList());
+
+    }
+}
